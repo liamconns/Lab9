a77fa9dcd6acea94744eef23b5a552279bf77117
Liam Connolly <liam.connolly@ku.edu>
2020-11-16 21:49:38 -0600

    update

--- exercise1/multiply.php
+++ exercise1/multiply.php
@@ -2,29 +2,32 @@
 
 echo "<table style=\"width:50%\">"; //creates a table in html
 
-for($i = 0; $i <= 100; $i++){
+for($i = 0; $i <= 100; $i++)
+{
 
 	echo "<tr>"; //rows
 
-	for($j = 0; $j <= 100; $j++){
-
+	for($j = 0; $j <= 100; $j++)
+	{
 		echo "<td>"; //cols
 
-		if($i == 0){
-
-			if($j != 0){
-
+		if($i == 0)
+		{
+			if($j != 0) 
+			{
 				echo "$j";
-			
 			}
-		} else if($j == 0){ //creates
-
-			echo "$i";
-			
-		} else {
+		} 
+		
+		else if($j == 0)
+		{ 
+			echo "$i";	
+		} 
+		
+		else 
+		{
 			echo "" . $i * $j . "";
 		}
-
 		echo "</td>";
 	}
 	echo "</tr>";
